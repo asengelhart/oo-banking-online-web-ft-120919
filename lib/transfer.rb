47f670cefd97819fb193a07c225a2a495415b359
Alex Engelhart <asengelhart@yahoo.com>
2020-01-02 19:18:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,6 +17,7 @@ class Transfer
   def execute_transaction
     if self.status == "pending" 
       if self.sender.balance < amount || self.sender.status != "open" || self.receiver.status != "open"
+        self.status = "rejected"
         return "Transaction rejected. Please check your account balance."
       else 
         self.sender.balance -= amount 
